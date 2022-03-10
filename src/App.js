dda09aa9d47bd369742891493117afe686c81221
JoshuaYoakum <joshua_yoakum@hotmail.com>
2022-03-10 14:24:08 -0800

    counter

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import logo from './logo.svg';
 import PersonCard from './componets/PersonCard';
 
 function App() {
-  const person = [{firstName:"Josh", lastName:"Yoakum", age:23, hair:"dark brown"},
-  {firstName:"Jersh", lastName:"yum", age:23, hair:"blonde"},
-  {firstName:"Joshua", lastName:"Yoak", age:13, hair:"brown"},
-  {firstName:"Joosh", lastName:"Yo", age:233, hair:"dark grey"}]
+  const person1 = {firstName:"Josh", lastName:"Yoakum", age:23, hair:"dark brown"}
+  const person2 ={firstName:"Jersh", lastName:"yum", age:23, hair:"blonde"}
+  const person3 ={firstName:"Joshua", lastName:"Yoak", age:13, hair:"brown"}
+  const person4 ={firstName:"Joosh", lastName:"Yo", age:233, hair:"dark grey"}
   return (
     <div className="App">
 
-    <PersonCard person={person}/>
+    <PersonCard person={person1}/>
+    <PersonCard person={person2}/>
+    <PersonCard person={person3}/>
+    <PersonCard person={person4}/>
     </div>
   );
 }
--- src/componets/PersonCard.jsx
+++ src/componets/PersonCard.jsx
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
     
     
 class PersonCard extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            count: 0, 
+            age: this.props.person.age
+        }
+    }
+    handleDecrementCount =()=>{
+        this.setAge({count:this.state.count-1})
+    }
     render() {
         const {person}=this.props
         return (
         <div>
-            {person.map((p,i)=><div key={i}><h1>{p.firstName}, {p.lastName}</h1>
-            <p>{p.age}</p>
-            <p>{p.hair}</p></div>)}
+            <h1>{person.firstName}, {person.lastName}</h1>
+            <p>{this.state.age}</p>
+            <p>{person.hair}</p>)
+            <button onClick={()=>this.setState({age: this.state.age+1})}>birthday</button>
         </div>);
     }
 }
